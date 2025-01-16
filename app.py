863dffd7b5dc5ab3dd449e852b986ddf64b3f08d
mtr <mtr@step.krg>
2025-01-16 19:20:16 +0300

    three commit

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
+import turtle
 from flask import (
     Flask, #библиотека фласк
     render_template, #компонент который переносит перемен в html
     redirect, #перенос данных
     request #запрос на шаблон или сайт
+
 )
 app = Flask(__name__) #инициализация
 
 @app.route('/base', methods=['GET', 'POST'])
 def get_base():
-  
+    
 
     return render_template('base.html')
 
 @app.route('/log', methods=['GET', 'POST'])
 def get_log():
-  
+
 
     return render_template('loger.html')
 
